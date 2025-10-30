b31b53ef57ce64b21c1b6315e7c3f6e28d6837a3
Suman-d97 <sumandass1811@gmail.com>
2025-10-30 12:42:01 +0530

    commit on next app

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React, { useState } from 'react'
+
+const SignUp = () => {
+    const [count, setCount] = useState(0);
+    console.log("count", count);
+    
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
